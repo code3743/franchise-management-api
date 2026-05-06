83504e35b167db87da3622aa2ceddff48ac3ebb3
code3743 <jota.lopez@correounivalle.edu.co>
2026-05-06 10:21:58 -0500

    Initialize franchise management API with Maven setup and basic application structure

---
+++ src/test/java/dev/jotalopez/franchise_management_api/FranchiseManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jotalopez.franchise_management_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FranchiseManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
